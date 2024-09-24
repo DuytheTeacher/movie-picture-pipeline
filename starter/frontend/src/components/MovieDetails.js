a9d95a142bda6452c2a7c9995cdcbdf94c4fe100
nguyenanhduy <nguyenanhduy0400@gmail.com>
2024-09-24 06:03:32 +0000

    Fix lint error

--- starter/frontend/src/components/MovieDetails.js
+++ starter/frontend/src/components/MovieDetails.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 export default function MovieDetails({ movie }) {
-    return <>
-        <h3>{ movie.title }</h3>
-        <p>{ movie.description }</p>
-    </>;
+  return (
+    <>
+      <h3>{movie.title}</h3>
+      <p>{movie.description}</p>
+    </>
+  );
 }
--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import axios from "axios";
+import axios from 'axios';
 
 export default function MovieList({ onMovieClick }) {
-    const [ movies, setMovies ] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-    useEffect(() => {
-        axios.get(`${ process.env.REACT_APP_MOVIE_API_URL }/movies`).then(res => setMovies(res.data.movies));
-    }, []);
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((res) => setMovies(res.data.movies));
+  }, []);
 
-    return <ul>
-        {
-            movies.map(item => <li key={ item.id } onClick={ () => onMovieClick(item) }>{ item.title }</li>)
-        }
-    </ul>;
+  return (
+    <ul>
+      {movies.map((item) => (
+        <li key={item.id} onClick={() => onMovieClick(item)}>
+          {item.title}
+        </li>
+      ))}
+    </ul>
+  );
 }
